34a067a806a68570edd3f26793cb4f65268b75b6
armgas45 <armgas461@gmail.com>
2024-03-18 15:26:51 +0400

    Initial commit

---
+++ src/main/java/com/kafka/kafkapostgresqlclient/KafkaPostgresqlClientApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkapostgresqlclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPostgresqlClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPostgresqlClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/kafkapostgresqlclient/KafkaPostgresqlClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkapostgresqlclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPostgresqlClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
